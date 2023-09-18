16b9a8df57e06d41d36bd06101de97d16b180ab9
Valentin Mrzv <bmvalentin27@gmail.com>
2023-09-18 21:46:06 +0300

    collections section

--- app/js/collections-content.js
+++ app/js/collections-content.js
@@ -1,74 +1,123 @@
-// fetch('./nft/content.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     // Создаем объект для хранения информации о том, сколько раз каждый пользователь упоминается
-//     const userCounts = {};
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('./nft/content.json')
+    .then(response => response.json())
+    .then(data => {
+      const userCounts = {};
 
-//     // Проходимся по каждому элементу в массиве данных
-//     data.forEach(item => {
-//       const userName = item.userName;
+      data.forEach(item => {
+        const userName = item.userName;
+        if (userName in userCounts) {
+          userCounts[userName]++;
+        } else {
+          userCounts[userName] = 1;
+        }
+      });
 
-//       // Если пользователь уже упоминался, увеличиваем его счетчик на 1, иначе устанавливаем счетчик в 1
-//       if (userCounts[userName]) {
-//         userCounts[userName]++;
-//       } else {
-//         userCounts[userName] = 1;
-//       }
-//     });
+      const topUsers = Object.entries(userCounts)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 3);
 
-//     // Находим пользователя с максимальным счетчиком
-//     let mostMentionedUser = null;
-//     let maxMentions = 0;
+      const collectionsList = document.querySelector('.collections__list');
 
-//     for (const userName in userCounts) {
-//       if (userCounts[userName] > maxMentions) {
-//         maxMentions = userCounts[userName];
-//         mostMentionedUser = userName;
-//       }
-//     }
+      topUsers.forEach(([userName, count]) => {
+        const liElement = document.createElement('li');
+        liElement.classList.add('collections__item');
 
-//     // Теперь у нас есть имя пользователя, который чаще всего упоминается
-//     console.log(`Пользователь, который чаще всего упоминается: ${mostMentionedUser}`);
+        const userItems = data.filter(item => item.userName === userName).slice(0, 3);
 
-//     // Теперь выбираем все объекты данных пользователя, где этот пользователь упоминается
-//     const userMentions = data.filter(item => item.userName === mostMentionedUser);
+        const userImageSrc = userItems[0].userImageSrc;
 
-//     // Выводим объекты данных пользователя
-//     console.log('Данные пользователя:', userMentions);
+        liElement.innerHTML = `
+        <div class="collections__grid">
+          <div class="collections-big" id="collections-swiper-big-${userName}">
+            <div class="collections-big__wrapper swiper-wrapper">
+            </div>
+          </div>
+          <div thumbsSlider="" class="collections-small" id="collections-swiper-small-${userName}">
+            <div class="collections-small__wrapper swiper-wrapper">
+            </div>
+          </div>
+        </div>
+        <h4 class="collections__subtitle">Amazing Collection</h4>
+        <div class="collections__inner">
+          <span>
+            <img src="${userImageSrc}" alt="" class="collections__user-img">
+            <p class="collections__text">
+              By&nbsp;
+              <span class="collections__name">${userName}</span>
+            </p>
+          </span>
+          <button class="collections__button main-btn main-btn--light">
+            Total&nbsp;
+            <span class="collections__quantity">${count}</span>&nbsp;Items
+          </button>
+        </div>
+      `;
 
-//     // Создаем новый массив с объектами данных пользователя
-//     const userArray = userMentions.map(item => ({
-//       id: item.id,
-//       imageSrc: item.imageSrc,
-//       imageSrcSet: item.imageSrcSet,
-//       nftName: item.nftName,
-//       userImageSrc: item.userImageSrc,
-//       bitValue: item.bitValue,
-//       stock: item.stock,
-//       userName: item.userName
-//     }));
+        collectionsList.appendChild(liElement);
 
-//     // Выводим новый массив с данными пользователя
-//     console.log('Массив с данными пользователя:', userArray);
-//   })
-//   .catch(error => {
-//     console.error('Ошибка при чтении файла content.json:', error);
-//   });
+        collectionsSwiper(userName, userItems);
+      });
+    })
+    .catch(error => {
+      console.error('Произошла ошибка при загрузке данных:', error);
+    });
+
+  // =========================================== COLLECTION SLIDER===================================
+  function collectionsSwiper(userName, userItems) {
 
 
-let collectionsBig = new Swiper('#collections-swiper-small', {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-let collectionsSmall = new Swiper('#collections-swiper-big', {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: collectionsBig,
-  },
+    const collectionsSmall = new Swiper(`#collections-swiper-small-${userName}`, {
+      spaceBetween: 16,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+      direction: "vertical",
+      slidesPerView: 3,
+      autoHeight: false,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    });
+    const collectionsBig = new Swiper(`#collections-swiper-big-${userName}`, {
+      spaceBetween: 0,
+      thumbs: {
+        swiper: collectionsSmall,
+      },
+      direction: "vertical",
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      simulateTouch: false,
+      touchRatio: 0,
+    });
+
+    userItems.forEach(item => {
+      const bigImage = document.createElement('div');
+      bigImage.classList.add('collections-big__slide', 'swiper-slide');
+      bigImage.innerHTML = `
+      <picture class="image">
+        <source srcset="${item.imageSrcSet[0]}" type="image/avif">
+        <source srcset="${item.imageSrcSet[1]}" type="image/webp">
+        <img srcset="${item.imageSrcSet[2]}" src="${item.imageSrc}" alt="">
+      </picture>
+    `;
+      const bigImageWrapper = document.querySelector(`#collections-swiper-big-${userName} .swiper-wrapper`);
+      bigImageWrapper.appendChild(bigImage);
+
+      const smallImage = document.createElement('div');
+      smallImage.classList.add('collections-small__slide', 'swiper-slide');
+      smallImage.innerHTML = `
+      <picture class="image">
+        <source srcset="${item.imageSrcSet[0]}" type="image/avif">
+        <source srcset="${item.imageSrcSet[1]}" type="image/webp">
+        <img srcset="${item.imageSrcSet[2]}" src="${item.imageSrc}" alt="">
+      </picture>
+    `;
+      const smallImageWrapper = document.querySelector(`#collections-swiper-small-${userName} .swiper-wrapper`);
+      smallImageWrapper.appendChild(smallImage);
+    });
+  }
 });
